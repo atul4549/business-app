a815106ba21ee571b2ce3275aa13c6c5b370fa6c
unknown <priyanshuprogrammer49@gmail.com>
2025-09-20 14:01:22 +0530

    added listen func in server folder

--- server/server.js
+++ server/server.js
@@ -8,3 +8,9 @@ dotenv.config()
 const app = express()
 const PORT  = process.env.PORT || 3000
 
+app.use(cors())
+app.use(express())
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on port ${PORT}`)
+})
