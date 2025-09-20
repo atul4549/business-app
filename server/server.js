4e396ca5ca2f5a2831bedd79bcd0f1d1cf5a1a7b
unknown <priyanshuprogrammer49@gmail.com>
2025-09-20 13:56:38 +0530

    installed vite@latest in client and written basic code in server file

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-kaka
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config()
+
+const app = express()
+const PORT  = process.env.PORT || 3000
+
