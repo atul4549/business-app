57f092faa367c6347039d668be0ac3f4597b464d
Atul Himanshu <priyanshuprogrammer49@gmail.com>
2025-09-20 16:29:04 +0530

    app.tsx clened

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
-hoi bhai
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
